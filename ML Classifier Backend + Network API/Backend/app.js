0a9a39e78f41c5278e29bf999c6a719398afcd8c
Tanishq <tanishqsaluja18@gmail.com>
2019-10-23 01:57:57 +0530

    integrated python script with nodejs backend

--- ML Classifier Backend + Network API/Backend/app.js
+++ ML Classifier Backend + Network API/Backend/app.js
@@ -1,16 +1,50 @@
 const express = require('express');
-let {PythonShell} = require('python-shell');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const {PythonShell} = require('python-shell');
 
 const app = express();
 
 // Middleware for POST request data 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+    res.send("Working");
+});
 
+app.post("/disease/diabetes",(req,res)=>{
 
+    console.log(req.body);
+    console.log(req.body.feat1);
+    console.log(req.body.feat2);  
+    console.log(req.body.feat3);  
+    console.log(req.body.feat4);   
+    console.log(req.body.feat5);   
+    console.log(req.body.feat6);  
+    console.log(req.body.feat7);   
+    console.log(req.body.feat8);   
 
-app.listen(4444,function(){
+    let options = {
+        "args":
+                [req.body.feat1,req.body.feat2,req.body.feat3,req.body.feat4,
+                req.body.feat5,req.body.feat6,req.body.feat7,req.body.feat8] 
+        }
+    
+    PythonShell.run("predict.py", options, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        output={
+            "outcome":result[0]
+        }
+        console.log(output);
+        res.json(output);
+    });
+});
+
+app.listen(process.env.PORT || 4444,function(){
     console.log('Server started');
 });
 
